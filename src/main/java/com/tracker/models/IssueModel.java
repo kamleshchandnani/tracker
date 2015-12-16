bd55bf9f3ef75e8f2565f4f2c246c936427e37d9
Kamlesh Chandnani <kamleshchandnani07@gmail.com>
2015-12-16 21:32:43 +0530

    model

--- src/main/java/com/tracker/models/IssueModel.java
+++ src/main/java/com/tracker/models/IssueModel.java
@@ -1,5 +1,68 @@
 package com.tracker.models;
 
+
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "issue_master")
 public class IssueModel {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(columnDefinition="serial")
+	private int ticket_id;
+
+	@Column(nullable = false, length = 30)
+	private String issue_title;
+	
+	@Column(nullable = false, columnDefinition="Text")
+	private String issue_desc;
+	
+	@Column(nullable=false,length=50)
+	private String created_by;
+	
+	@Column(nullable=false,length=12)
+	private String issue_status;
+
+	public int getTicket_id() {
+		return ticket_id;
+	}
+
+	public void setTicket_id(int ticket_id) {
+		this.ticket_id = ticket_id;
+	}
+
+	public String getIssue_title() {
+		return issue_title;
+	}
+
+	public void setIssue_title(String issue_title) {
+		this.issue_title = issue_title;
+	}
+
+	public String getIssue_desc() {
+		return issue_desc;
+	}
+
+	public void setIssue_desc(String issue_desc) {
+		this.issue_desc = issue_desc;
+	}
+
+	public String getCreated_by() {
+		return created_by;
+	}
+
+	public void setCreated_by(String created_by) {
+		this.created_by = created_by;
+	}
+
+	public String getIssue_status() {
+		return issue_status;
+	}
+
+	public void setIssue_status(String issue_status) {
+		this.issue_status = issue_status;
+	}
 
 }
--- src/main/java/com/tracker/models/UserModel.java
+++ src/main/java/com/tracker/models/UserModel.java
@@ -1,5 +1,45 @@
 package com.tracker.models;
 
-public class UserModel {
+import javax.persistence.*;
 
+@Entity
+@Table(name = "user_master")
+public class UserModel {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(columnDefinition = "serial")
+	private int u_id;
+	
+	@Column(nullable = false, length = 30)
+	private String u_name;
+
+	@Column(nullable = false, length = 30)
+	private String u_password;
+
+
+	public int getU_id() {
+		return u_id;
+	}
+
+	public void setU_id(int u_id) {
+		this.u_id = u_id;
+	}
+
+	public String getU_name() {
+		return u_name;
+	}
+
+	public void setU_name(String u_name) {
+		this.u_name = u_name;
+	}
+
+	public String getU_password() {
+		return u_password;
+	}
+
+	public void setU_password(String u_password) {
+		this.u_password = u_password;
+	}
+
+	
 }
